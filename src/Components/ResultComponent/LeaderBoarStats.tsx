d842a26bc82fdf53523e033d44b85dec1fe1f5b6
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-05-24 16:31:11 +0530

    deployed backend and connected leaderboard  stats to frontend

--- src/Components/ResultComponent/LeaderBoarStats.tsx
+++ src/Components/ResultComponent/LeaderBoarStats.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -7,51 +7,67 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 550,
-  },
-});
-
-function createData(
-  Rank: Number,
-  Name: string,
-  Score: number,
-  Category: number,
-  Avatar: string
-) {
-  return { Rank, Name, Score, Category, Avatar };
-}
-
-const rows = [
-  createData(
-    1,
-    "Akshay",
-    6.0,
-    24,
-    "https://avatars.dicebear.com/api/male/.svg"
-  ),
-  createData(
-    2,
-    "kal-el",
-    9.0,
-    37,
-    "https://avatars.dicebear.com/api/female/.svg"
-  ),
-  createData(
-    3,
-    "steve",
-    16.0,
-    24,
-    "https://avatars.dicebear.com/api/jdenticon/.svg"
-  ),
-  createData(4, "tony", 3.7, 67, "https://avatars.dicebear.com/api/initials/.svg"),
-  createData(5, "thor", 16.0, 49, "https://avatars.dicebear.com/api/bottts/.svg"),
-];
+import { useQuizContext } from "../../Contexts/QuizContext/QuizContext";
+import axios from "axios";
 
 export default function DenseTable() {
+  type leaderBoardDesc = {
+    rank: number;
+    name: string;
+    score: number;
+    category: number;
+    avatar: string;
+  };
+
+  type leaderBoardType = {
+    result?: "string";
+    message?: "string";
+    data: leaderBoardDesc[];
+  };
+
+  const [leaderBoardData, setLeaderBoardData] = useState<leaderBoardType>();
+  const { quizState } = useQuizContext();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const urlData = {
+          name: quizState.userName,
+          category: quizState.categorySelected,
+          score: quizState.currentScore,
+          avatar: quizState.userAvatar,
+        };
+        const data = await axios.post(`https://lit-taiga-43779.herokuapp.com/`, urlData);
+        setLeaderBoardData(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  console.log(leaderBoardData);
+
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 550,
+    },
+  });
+
   const classes = useStyles();
+
+  function createData(
+    Rank: Number,
+    Name: string,
+    Score: number,
+    Category: number,
+    Avatar: string
+  ) {
+    return { Rank, Name, Score, Category, Avatar };
+  }
+
+  const rows = leaderBoardData?.data?.map((ele, index) =>
+    createData(index, ele.name, ele?.score, ele?.category, ele?.avatar)
+  );
 
   return (
     <TableContainer component={Paper}>
@@ -66,22 +82,24 @@ export default function DenseTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow>
-              <TableCell align="left">{row.Rank}</TableCell>
-              <TableCell align="right">{row.Name}</TableCell>
-              <TableCell align="right">{row.Score}</TableCell>
-              <TableCell align="right">{row.Category}</TableCell>
-              <TableCell align="center">
-                {" "}
-                <img
-                  style={{ width: "15%", borderRadius: "50%" }}
-                  src={row.Avatar}
-                  alt=""
-                />
-              </TableCell>
-            </TableRow>
-          ))}
+          {rows != undefined &&
+            rows?.length > 0 &&
+            rows?.map((row) => (
+              <TableRow>
+                <TableCell align="left">{row?.Rank}</TableCell>
+                <TableCell align="right">{row?.Name}</TableCell>
+                <TableCell align="right">{row?.Score}</TableCell>
+                <TableCell align="right">{row?.Category}</TableCell>
+                <TableCell align="center">
+                  {" "}
+                  <img
+                    style={{ width: "15%", borderRadius: "50%" }}
+                    src={row.Avatar}
+                    alt=""
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </TableContainer>
